d0d7c355555227b647668a079532d291aca55220
NoaBenitezPXL <12000080@student.pxl.be>
2022-09-19 12:33:56 +0200

    for loops

--- Opdracht1/opdracht01.js
+++ Opdracht1/opdracht01.js
@@ -1,5 +1,12 @@
 console.log("Welkom in de wondere wereld van Web Expert");
 
+let names = ["tomas", "bram", "marie"]
+names.forEach((name) => console.log(name));
+
+for (const name of names){
+    console.log(name);
+}
+
 let test = "test";
 console.log("de variabele = " + test + ". dit is een test.");
 console.log(`de variabele = ${test}. dit is een test.`);
