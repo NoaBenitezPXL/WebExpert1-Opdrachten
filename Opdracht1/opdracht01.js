8d260e57b9f143ec4a24d0b79617cff2d48c9370
NoaBenitezPXL <12000080@student.pxl.be>
2022-09-19 12:22:20 +0200

    test

--- Opdracht1/opdracht01.js
+++ Opdracht1/opdracht01.js
@@ -1,4 +1,5 @@
 console.log("Welkom in de wondere wereld van Web Expert");
 
 let test = "test";
+console.log("de variabele = " + test + ". dit is een test.");
 console.log(`de variabele = ${test}. dit is een test.`);
